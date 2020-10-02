5e86acee9b3925c215734c0127c59b5095ebf6b0
Justin5021 <jc.wk01@gmail.com>
2020-10-02 23:42:05 +0800

    Composer and MPDF
    - Installed a library to add a pdf generating class for php
    - Yea its huge (￣ω￣;)
    - Comitting for the safety of my sanity

--- ViewSubscription-Report.php
+++ ViewSubscription-Report.php
@@ -1,26 +1,3 @@
 ﻿<?php
-session_start(); 
-
-$dbhost='db4free.net';
-$dbuser='siteuser';
-$dbpass='securepassword';
-$dbname='telehealth';
-
-$dbc = new mysqli($dbhost, $dbuser, $dbpass, $dbname)
-or die('Could not connect %s\n'. $dbc->connect_error);
-
-require('fpdf.php');
-
-$pdf = new FPDF('P', 'mm', 'A4');
-
-$pdf->AddPage();
-
-// Font of the Page
-$pdf->SetFont('Arial', 'B', 14);
-
-$pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
-
-
 
 ?>
