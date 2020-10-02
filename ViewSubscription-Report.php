c65a3de794cd38af429e080808e5b54c9486d200
Justin5021 <jc.wk01@gmail.com>
2020-10-03 02:55:37 +0800

    PDF File
    - Work in Progress
    - Trying to figure out how to display contents from database onto the pdf

--- ViewSubscription-Report.php
+++ ViewSubscription-Report.php
@@ -1,8 +1,34 @@
 ﻿<?php
+// to fix the output data error
+ob_start();
 require('vendor/webeweb/fpdf-library/fpdf.php');
+
+// Database Connection
+$dbhost='db4free.net';
+$dbuser='siteuser';
+$dbpass='securepassword';
+$dbname='telehealth';
+
+$dbc = new mysqli($dbhost, $dbuser, $dbpass, $dbname)
+or die('Could not connect %s\n'. $dbc->connect_error);
+
+//Data
+$formid = $_GET['FormID'];
+
+//$query = "SELECT * FROM MedSubscriptions WHERE FormID = '$formid'";
+$query = mysqli_query($dbc, "SELECT * FROM MedSubscriptions WHERE FormID = '$formid'");
+//$query = mysqli_query($dbc, "SELECT * FROM MedSubscriptions WHERE FormID = '".$_GET['FormID']."'");
+$report = mysqli_fetch_array($query);
+
 $pdf = new FPDF('P', 'mm', 'A4');
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,30, $report['FormID']);
+
+// Output the PDF
 $pdf->Output();
+
+// to fix the output data error
+ob_end_flush();
 ?>
